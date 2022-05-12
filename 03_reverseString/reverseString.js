08214083437e85aa0bafc58a99fc9aa7ab500642
kitokta <96595360+kitokta@users.noreply.github.com>
2022-05-12 16:49:32 -0300

    Meus exercicios

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+myString ='';
 
+for (let i=string.length-1; i>=0; i--){
+    myString = myString + string[i];
+}
+
+return myString;
 };
 
+reverseString('')
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+   let myArray = args[0];
+   let newArray = [];
+    myArray.forEach(item => {
+    if(!args.includes(item)){
+            newArray.push(item);
+        }
+    });
+        
+return newArray;
+}
 
-};
-
+removeFromArray([1,2,3,4], 3)
 // Do not edit below this line
 module.exports = removeFromArray;
