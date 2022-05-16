0f42d6cf22ec554bc666320d5bfee1dd43cff9d8
Gabi Zanini <psicologia.plus@gmail.com>
2022-05-16 16:38:31 -0300

    05_sumAll test done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,45 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    let endSum = 0;
+    let error ="";
+
+    if (startNum < 0 || endNum <0){
+        return error='ERROR';
+    }
+
+    else if (typeof(startNum)!='number' || typeof(endNum)!='number'){
+        return error='ERROR';
+}
+
+    else{
+        if (startNum<endNum){
+            while (startNum<=endNum){
+        endSum = endSum +startNum;
+        startNum++;
+     }
+            return endSum;
+}
+        else if (startNum>endNum){
+            while (startNum>=endNum){
+            endSum = endSum +startNum;
+            startNum--;
+         }
+            return endSum;
+    }
+}
+    
+
+
+
+
+
+// for (let i = startNum; i<=endNum; i++){
+    //endSum = endSum +i;}
+
+
 
 };
 
+
+sumAll(-10, 4);
 // Do not edit below this line
 module.exports = sumAll;
