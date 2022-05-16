25395c6fde0b3c3321ed23b90e4ae33108483b01
Gabi Zanini <psicologia.plus@gmail.com>
2022-05-16 17:43:24 -0300

    test done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,22 @@
-const ftoc = function() {
+const ftoc = function(temp) {
+let newTemp = (temp-32) * (5/9);
 
+if (newTemp%1!=0) newTemp = newTemp.toFixed(1);
+
+return parseFloat(newTemp);
 };
 
-const ctof = function() {
+const ctof = function(temp) {
+  let newTemp = temp * (9/5) + 32;
 
+  if (newTemp%1!=0) newTemp = newTemp.toFixed(1);
+  
+  return parseFloat(newTemp);
+  ;
 };
+
+ftoc ();
+ctof();
 
 // Do not edit below this line
 module.exports = {
