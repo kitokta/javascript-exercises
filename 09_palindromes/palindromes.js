0810b9bd8e80f8df5a7ea9d3eb6d0f7b1371ecab
Gabi Zanini <psicologia.plus@gmail.com>
2022-05-30 14:17:04 -0300

    Could'nt make 09 test to work

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+const reverseString = string.split('').reverse().join('');
+const length = string.length;
+let result = 0;
+for (let i =0; i<length;i++) {
+    reverseString[i] === string[i] ? result += 0 : result += 1;
+}
+if (result != 0) return false;
+else return true;
 
 };
 
