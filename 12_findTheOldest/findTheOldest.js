7497692d498b38987631c02f1eeddc5a522016b7
Gabi Zanini <psicologia.plus@gmail.com>
2022-05-30 15:53:22 -0300

    Exercise 12 done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+ return people.reduce((lastPerson, currentPerson) => {
+     const lastPersonBirthday = getAge(lastPerson.yearOfBirth, lastPerson.yearOfDeath);
+     const currentPersonBirthday = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+     
+     return (lastPersonBirthday > currentPersonBirthday) ? lastPerson : currentPerson;
+ })
 
+ function getAge(birthday, death){
+     if(!death) {
+         death = new Date().getFullYear();
+     }
+     return death - birthday;
+ }
 };
 
 // Do not edit below this line
