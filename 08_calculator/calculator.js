49f06a4b0bb9b0590b5e34944a0e38aa23fa3b3a
Gabi Zanini <psicologia.plus@gmail.com>
2022-05-30 13:12:47 -0300

    Passed last 08 test

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -17,13 +17,23 @@ const multiply = function(array) {
     return total*currentNumber})
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	let power = num1;
+  for (let i=1; i<num2; i++) {
+    power *= num1
+  }
+return power;
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(num) {
+	if (num === 1 || num === 0) return 1;
+  else {
+    let result = num;
+  for (let i = num-1; i >=1; i--) {
+    result *= i;
+  }
+  return result;
+}};
 
 // Do not edit below this line
 module.exports = {
